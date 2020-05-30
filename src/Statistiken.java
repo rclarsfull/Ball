fbe1f2464b4e20fd74c71b52ab50e1a123402876
rclarsfull <l.p.1999@live.de>
2020-05-30 13:19:13 +0200

    Personen auf tische verteilen repariert

---
+++ src/Statistiken.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Statistiken implements java.io.Serializable{
+    public static void auswahl(Ball veranstaltung){
+        Scanner eingabe=new Scanner(System.in);
+        String [] menuepunkte= {"Angemeldete Personen","Maximales Platangebot","Freie Plaetze","Einnahmen","Zurueck"};
+
+        boolean wid=true;
+        while (wid) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("Waehlen sie mithilfe der Nummer den Menuepunkt aus.");
+            for (int i = 0; i < menuepunkte.length; i++) {
+                System.out.println((i + 1) + ". " + menuepunkte[i]);
+            }
+
+            switch (eingabe.nextInt()) {
+                case 1:
+                    MenuePersonen.auswahl(veranstaltung);
+                    break;
+                case 2:
+                    MenueTische.auswahl(veranstaltung);
+                    break;
+                case 3:
+                    wid=false;
+                    Speichern.ueberschreiben(veranstaltung);
+                    break;
+                default:
+                    System.out.println("Falsche Eingabe!!");
+                    break;
+            }
+        }
+
+    }
+}
